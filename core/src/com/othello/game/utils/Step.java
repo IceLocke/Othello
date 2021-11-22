a8bacab059785260e0b0fce2b9d55ee334f84daf
sustechkl <12111012@mail.sustech.edu.cn>
2021-11-22 13:43:05 +0800

    完成了core除addstep以外的内容 没有更新position啊，不知道为什么也要commit

--- core/src/com/othello/game/utils/Step.java
+++ core/src/com/othello/game/utils/Step.java
@@ -1,4 +1,35 @@
 package com.othello.game.utils;
 
 public class Step {
+    private int x, y, color;
+
+    public Step(int x, int y, int color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
 }
